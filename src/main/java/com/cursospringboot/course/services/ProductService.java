2a9e3f19a1c350df4adf2414a15f76efbea4d56c
caua-ananias <caua.ananias2004@gmail.com>
2025-02-07 21:15:41 -0400

    Product

---
+++ src/main/java/com/cursospringboot/course/services/ProductService.java
@@ -0,0 +1,25 @@
+package com.cursospringboot.course.services;
+
+import com.cursospringboot.course.entities.Product;
+import com.cursospringboot.course.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepository repository;
+
+    public List<Product> findAll() {
+        return repository.findAll();
+    }
+
+    public Product findById(Long id) {
+        Optional<Product> obj = repository.findById(id);
+        return obj.get();
+    }
+}
